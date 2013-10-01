175fc4d88c35172e404d80f892ecc3efc47d67f2
Seer82 <mp_cartwright@hotmail.com>
2013-10-01 15:50:56 +0100

    update

--- BaseAdapters/src/com/example/baseadapters/MainActivity.java
+++ BaseAdapters/src/com/example/baseadapters/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -14,37 +15,35 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-	ListView listView;
-
+	ListView MylistView;
 	Context context;
-	AlertDialog dialog;
+	AlertDialog dialog1;
+	ArrayAdapter<String> MyListadapter;
 	String Data1;
-
-	ArrayList<String> array = new ArrayList<String>();
+    ArrayList<String> Myarray = new ArrayList<String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		array.add("mark");
-		array.add("colin");
-		
-		
-		String[] stuff = array.toArray(new String[array.size()]);// setting the array list to a String Array
+		Myarray.add("mark");
+		Myarray.add("colin");
+		Myarray.add("modo");
 
 		context = this;
-		listView = (ListView) findViewById(R.id.ListView);
+		MylistView = (ListView) findViewById(R.id.ListView);
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.activity_main, R.id.textView1, stuff);
-		listView.setAdapter(adapter);
+		MyListadapter = new ArrayAdapter<String>(this, R.layout.activity_main,
+				R.id.textView1, Myarray);
+		MylistView.setAdapter(MyListadapter);
 
-		listView.setOnItemClickListener(new Listener());
+		MylistView.setOnItemClickListener(new Listener());
 
 	}
 
@@ -53,24 +52,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+
 	}
 
 	private class Listener implements OnItemClickListener {
 
 		@Override
-		public void onItemClick(AdapterView<?> adapter, View view,
+		public void onItemClick(final AdapterView<?> adapter1, View view,
 				int position, long arg3) {
-
-			// TextView textView = (TextView) view.findViewById(R.id.textView1);
-
-			// String text = textView.getText().toString();
-
-			/*
-			 * Toast.makeText(context, text + " clicked at position " +
-			 * position, Toast.LENGTH_LONG).show();
-			 */
-			Scanner scanner = new Scanner(System.in);
-			Data1 = scanner.toString();
 
 			AlertDialog.Builder builder = new AlertDialog.Builder(context);
 			LayoutInflater inflater = (LayoutInflater) context
@@ -79,6 +68,7 @@ public class MainActivity extends Activity {
 			// Inflate and set the layout for the dialog
 			// Pass null as the parent view because its going in the dialog
 			// layout
+
 			builder.setView(inflater.inflate(R.layout.custom_dialogue, null))
 
 					// Add action buttons
@@ -88,9 +78,20 @@ public class MainActivity extends Activity {
 								public void onClick(DialogInterface dialog,
 										int id) {
 
-									array.add(Data1);
+									final EditText text = (EditText) dialog1
+											.findViewById(R.id.EditText1);
+									String Data1 = text.getText().toString();
+									System.out.println(Data1);
+
+									Myarray.add(Data1);// adding input string
+														// from Edit Text to
+														// array
+									MyListadapter.notifyDataSetChanged();
+									
+									
 
 								}
+
 							})
 					.setNegativeButton(R.string.cancel,
 							new DialogInterface.OnClickListener() {
@@ -101,8 +102,8 @@ public class MainActivity extends Activity {
 
 								}
 							});
-			dialog = builder.create();
-			dialog = builder.show();
+			dialog1 = builder.create();
+			dialog1 = builder.show();
 
 		}
 
